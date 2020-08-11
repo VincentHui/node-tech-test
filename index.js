2490ff636b2ea5569f0e583a93a21237419ed2b0
VincentHui <VincentHui@hotmail.co.uk>
2020-08-11 10:34:19 +0100

    adding place holder instead of hello world

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 const makeRequest = require("./collection");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Human readable response!");
 });
 
 app.listen(port, () => {
