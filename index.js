c5b9e48b59bad47a75ef36fc20102438ef75858c
VincentHui <VincentHui@hotmail.co.uk>
2020-08-11 10:42:04 +0100

    adding in memory store

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const makeRequest = require("./collection");
 
+jsonOutput = [];
+
 app.get("/", (req, res) => {
   res.send("Human readable response!");
 });
@@ -13,6 +15,7 @@ app.listen(port, () => {
 
 setInterval(function () {
   //want to make this a list of promises to resolve and concat into one array and store in memory
+  //should be stored in jsonOutput object
   makeRequest("http://test1.infra.getlenses.co.uk/", (res) => {});
   makeRequest("http://test2.infra.getlenses.co.uk/", (res) => {});
   makeRequest("http://test3.infra.getlenses.co.uk/", (res) => {});
