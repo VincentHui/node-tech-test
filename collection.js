e614a6af594aaaacbf5eb85b454be7caa76609b0
VincentHui <VincentHui@hotmail.co.uk>
2020-08-11 12:44:25 +0100

    can now collect and flatten results

--- collection.js
+++ collection.js
@@ -2,10 +2,16 @@ var http = require("http");
 const axios = require("axios");
 
 const makeRequest = async (url) => axios.get(url);
-(async () => {
-  const response = await makeRequest("http://test3.infra.getlenses.co.uk/");
-  console.log(response.data);
-})();
 
-// console.log(response);
-module.exports.makeRequest = makeRequest;
+const collect = async () => {
+  const responses = await axios.all([
+    makeRequest("http://test1.infra.getlenses.co.uk/"),
+    makeRequest("http://test2.infra.getlenses.co.uk/"),
+    makeRequest("http://test3.infra.getlenses.co.uk/"),
+  ]);
+  return responses.reduce((acc, r) => {
+    return acc.concat(r.data);
+  }, []);
+};
+
+module.exports.collect = collect;
