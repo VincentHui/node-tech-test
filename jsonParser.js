c29fa862bbb9f397f3ad260398411b13f226817c
VincentHui <VincentHui@hotmail.co.uk>
2020-08-11 10:36:53 +0100

    adding in very basic parsing structure

---
+++ jsonParser.js
@@ -0,0 +1,18 @@
+const testAray = [
+  {
+    ID: "ad3jlDFsdafF",
+    Priority: "4",
+    Category: "A",
+    Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  },
+  {
+    ID: "ad3jlDFsdafF",
+    Priority: "3",
+    Category: "C",
+    Description: "Lorem ipsum dolor sit amet",
+  },
+];
+
+export function parseJson(jsonArray) {
+  return "human readable response";
+}
